790aa324c050d9dd399975457c4772da15cc888a
FreddieLui <luiszekwan>
2019-02-13 22:57:40 +0800

    Update Tic-Tac-Toe_Pascal.pas

--- Tic-Tac-Toe_Pascal.pas
+++ Tic-Tac-Toe_Pascal.pas
@@ -1,26 +1,36 @@
 
 Var status : array[1..3,1..3] Of char;
-    player : integer;//p0_p1
-    x, y : integer; //current position
+  player : integer;
+  //p0_p1
+  x, y : integer;
+  //current position
 
-Function wincheck(status:Array OF CHAR) : boolean;
+Function wincheck(status:Array Of CHAR) : boolean;
 Begin
-  If (status[1,1] = status[1,2]) And (status[1,2]= status[1,3])
-     Else If (status[2,1] = status[2,2]) And (status[2,2]= status[2,3])
-     Else If (status[3,1] = status[3,2]) And (status[3,2]= status[3,3])
-     Else If (status[1,1] = status[2,1]) And (status[2,1]= status[3,1])
-     Else If (status[1,2] = status[2,2]) And (status[2,2]= status[3,2])
-     Else If (status[1,3] = status[2,3]) And (status[2,3]= status[3,3])
-     Else If (status[1,1] = status[2,2]) And (status[2,2]= status[3,3])
-     Else If (status[3,1] = status[2,2]) And (status[2,2]= status[1,3])
-    Then wincheck := true;
+  If (status[1,1] = status[1,2]) And (status[1,2]= status[1,3]) Then 
+          wincheck := true
+  Else If (status[2,1] = status[2,2]) And (status[2,2]= status[2,3]) Then
+          wincheck := true
+  Else If (status[3,1] = status[3,2]) And (status[3,2]= status[3,3]) Then
+          wincheck := true
+  Else If (status[1,1] = status[2,1]) And (status[2,1]= status[3,1])Then
+          wincheck := true
+  Else If (status[1,2] = status[2,2]) And (status[2,2]= status[3,2])Then
+          wincheck := true
+  Else If (status[1,3] = status[2,3]) And (status[2,3]= status[3,3])Then
+           wincheck := true
+  Else If (status[1,1] = status[2,2]) And (status[2,2]= status[3,3])Then
+           wincheck := true
+  Else If (status[3,1] = status[2,2]) And (status[2,2]= status[1,3])
+         Then wincheck := true;
   Else
 End;
 
-procedure switchplayer(player);
-begin
-  if player = 0 then player :=1 else if player = 1 then player := 0;
-end;
+Function switchplayer(player);
+Begin
+  If player = 0 Then player := 1
+  Else If player = 1 Then player := 0;
+End;
 
 
 Procedure writeboard
@@ -35,6 +45,20 @@ Begin
     End;
 End;
 
+Function select(x1,y1:integer);
+//
+Begin
+if readkey = 
+
+then 
+
+elseif 
+
+then 
+
+
+
+End;
 
 //MAIN
 Begin
@@ -42,18 +66,21 @@ Begin
   readln();
   writeboard();
 
-While wincheck := false Do
-begin
-  writeboard();
-  wincheck();
-end;
-  
-while wincheck= true Do
-begin 
-  writeln(player,'WINS');
+
+  While wincheck := false Do
+    Begin
+      writeboard;
+      select;
+      wincheck;
+      switchplayer;
+    End;
+
+  While wincheck= true Do
+    Begin
+      writeln(player,'WINS');
+      readln;
 
 
-
-
+    End;
 
 End.
