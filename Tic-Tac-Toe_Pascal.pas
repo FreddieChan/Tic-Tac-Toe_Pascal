2801a9d213f4b97175af56ec299fa5503119afde
FreddieLui <luiszekwan@gmail.com>
2019-02-19 23:58:01 +0800

    .

--- Tic-Tac-Toe_Pascal.pas
+++ Tic-Tac-Toe_Pascal.pas
@@ -4,43 +4,16 @@ Uses sysutils, crt;
 Var status :   array[1..3,1..3] Of char;
     player :   integer;
     x1, y1 :   integer;
-    //current position
     f :   text;
     playno :   integer;
     key :   char;
+    wincheck:   boolean;
+    selected :array[1..3,1..3] Of char;
 
 Type 
     a =   array[1..3,1..3] Of char;
 
-Procedure writefile(f:text);
-
-Var i,j :   integer;
-Begin
-    assign(f, inttostr(playno) + '.txt');
-    rewrite(f);
-    For i := 1 To 3 Do
-        Begin
-            For j := 1 To 3 Do
-                writeln(status[i,j]);
-        End;
-    close(f);
-
-End;
-
-procedure resetboard(params;
-begin
-    Var i:   char;
-
-Begin
-    For i := a to z do begin
-    
-        End;
-End;
-
-end;
-
-
-Function wincheck(status:a) :   boolean;
+Procedure winning;
 Begin
     If (status[1,1] = status[1,2]) And (status[1,2]= status[1,3]) Then
         wincheck := true
@@ -60,6 +33,42 @@ Begin
              Then wincheck := true;
 End;
 
+
+Procedure writefile;
+
+Var i,j :   integer;
+Begin
+    assign(f, inttostr(playno) + '.txt');
+    rewrite(f);
+    For i := 1 To 3 Do
+        Begin
+            For j := 1 To 3 Do
+                writeln(status[i,j]);
+        End;
+    close(f);
+
+End;
+
+
+Procedure resetgame;
+
+Var i, j :   integer; k:   char;
+Begin
+    clrscr;
+    wincheck := false;
+    For i := 1 To 3 Do
+        Begin
+            For j := 1 To 3 Do
+                Begin
+                    For k := 'a' To 'j' Do
+                        status[i,j] := k;
+                End;
+        End;
+
+End;
+
+
+
 Procedure switchplayer(player:integer);
 Begin
     If player = 0 Then
@@ -73,6 +82,8 @@ Begin
                  key := 'O';
              End
 End;
+
+
 
 Procedure writeboard;
 
@@ -91,25 +102,23 @@ End;
 
 Function select(x1,y1:integer):   a ;
 
-Var k :   char;
-
-    //missing x1y1 initialization,range
+Var k :   integer;
 Begin
     Repeat
         k := ord(readkey);
         //ord value #
-        While ((x1>1) And (x1<3)) and ((y1>1) And (y1<3)) Do
+        While ((x1>1) And (x1<3)) And ((y1>1) And (y1<3)) Do
             Begin
-                If k = 'A' Then x1 := x1-1
+                If k = 97 Then x1 := x1-1
                 Else
-                    If k = 'D' Then x1 := x1+1
+                    If k = 110 Then x1 := x1+1
                 Else
-                    If k = 'W' Then y1 := y1+1
+                    If k = 119 Then y1 := y1+1
                 Else
-                    If readkey = 'S' Then y1 := y1-1;
+                    If k = 115 Then y1 := y1-1;
             End
-    Until readkey = #10;
-    select[1,1] := [x1,y1] ;
+    Until k = 13;
+    selected := [x1,y1];
 
 End;
 
@@ -121,16 +130,16 @@ Begin
     While wincheck = false Do
         Begin
             writeboard;
-            select;
-            gotoxy(select);
-            
-            If status(select) = '' Then
-                write(key,status(select));
-            wincheck;
+            selected;
+            gotoxy(selected);
+
+            If status(selected) = '' Then
+                write(key,status(selected));
+            winning;
             switchplayer;
         End;
 
-    While wincheck= true Do
+    While wincheck Do
         Begin
             writeln(player,'WINS');
             writefile;
@@ -139,9 +148,9 @@ Begin
             wincheck := false;
 
         End;
-    
-    While movecnt = 9 then 
-     writeln('TIE');
+
+    While movecnt = 9 Do
+        writeln('TIE');
 
     readln;
 End.
